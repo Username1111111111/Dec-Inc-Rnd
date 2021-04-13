421f87699aa62db1d50af346a7458285eaf8fb0d
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2021-04-13 03:50:18 +0300

    Pure redux practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//reducer must be free from calculations and depend only from input action and state;
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+  case 'INC':
+    return state + 1;
+  case 'DEC': 
+    return state - 1;
+  case 'RND': 
+    return state + action.value;
+  default:
+    return state;
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// let state = reducer(undefined, {});
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
+
+//action creators
+const inc = () => ({ type: 'INC'});
+const dec = () => ({ type: 'DEC'});
+const rnd = (value) => ({ type: 'RND', value});
+
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10 );
+  // input values here, inside action:
+  store.dispatch(rnd(value));
+});
+
+// document.getElementById('rnd').addEventListener('click', () => {
+//   const value = Math.floor(Math.random() * 10 );
+//   // input values here, inside action:
+//   store.dispatch({type: 'RND', value});
+// });
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+};
+
+store.subscribe(update);
+
+// Subscribe before action dispatch. Executes after state change (action dispatch which changed state)
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
+
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+
+
