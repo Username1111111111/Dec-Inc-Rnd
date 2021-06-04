5b97184ee455ebd9960b0678eae520b1771a718c
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2021-06-04 18:24:00 +0300

    Added README

--- src/index.js
+++ src/index.js
@@ -8,13 +8,6 @@ import App from './components/app';
 
 const store = createStore(reducer);
 
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// };
-
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
 // document.getElementById('inc').addEventListener('click', inc);
 // document.getElementById('dec').addEventListener('click', dec);
 // document.getElementById('rnd').addEventListener('click', () => {
